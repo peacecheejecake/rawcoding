df6a82a0de45f568a448ec78a28dcf7a0a3678d0
Jiwon <peace.cheejecake@gmail.com>
2021-02-10 19:10:04 +0900

    Inital Commit

---
+++ abalone/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import csv
+import os.path
+
+
+# hyperparameters
+LEARNING_RATE = 1e-3
+
+# data chracteristics
+OUTPUT_COLS = 1
+DATA_PATH = ['..', 'data']
+DATA_FILE_NAME = 'abalone.csv'
