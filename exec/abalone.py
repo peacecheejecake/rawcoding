9ff6dca7dfc86d5015cd8b5472c1a8f0b1dbe453
MIN JIWON <jiwon@Jiwons-Macmini.local>
2021-02-10 23:18:37 +0900

    Make exec package

--- exec/abalone.py
+++ exec/abalone.py
@@ -1,46 +1,32 @@
-import path
-
 import numpy as np
-import csv
-import os.path
 
+from path import *
 from utils import read_csv, one_hot
 from utils import DataIterator
 from nn.model import SingleLayerPerceptron
 
-
-# hyperparameters
-LEARNING_RATE = 1e-3
-EPOCHS = 10
-BATCH_SIZE = 10
-
-TEST_RATIO = 0.1
 
 # data chracteristics
 output_cols = 1
 data_path = ['..', 'data']
 data_file_name = 'abalone.csv'
 
-# report settings
-report_every = 1
-test_size = 50
-title_width = 40
 
-
-def load_abalone_dataset(test_ratio=TEST_RATIO):
+def load_abalone_dataset(test_ratio=0.1):
     raw_data = read_csv(data_path, data_file_name, False)
     data = one_hot(raw_data, [0]).astype(np.float64)
     test_size = int(data.shape[0] * test_ratio)
     return data[:-test_size], data[-test_size:]
 
 
-if __name__ == "__main__":
+def main(lr=1e-3, epochs=10, batch_size=10, test_ratio=0.1, test_size=50, \
+        title_width=40, report_every=1):
     # data arrangement
-    train_data, test_data = load_abalone_dataset(test_ratio=TEST_RATIO)
+    train_data, test_data = load_abalone_dataset(test_ratio=test_ratio)
     train_iter = DataIterator(data=train_data, output_dim=output_cols, \
-        batch_size=BATCH_SIZE)
+        batch_size=batch_size)
     test_iter  = DataIterator(data=test_data, output_dim=output_cols, \
-        batch_size=BATCH_SIZE)
+        batch_size=batch_size)
 
     # create a model & initialize parameters
     Model = SingleLayerPerceptron(name='slp', xdim=train_iter.xdim, ydim=train_iter.ydim)
@@ -53,15 +39,15 @@ Test Accuracy: [{Model.evaluate(test_iter)}]", end='\n\n')
 
     # train
     print(" Start of Training ".center(title_width, '='))
-    for epoch in range(1, EPOCHS + 1):
+    for epoch in range(1, epochs + 1):
         loss_epoch_sum = 0
         for batch_in, batch_out in train_iter:
-            loss_epoch_sum += Model.backward(batch_in, batch_out, LEARNING_RATE)
+            loss_epoch_sum += Model.backward(batch_in, batch_out, lr)
         loss = loss_epoch_sum / len(train_data)
 
         train_accuracy = Model.evaluate(train_iter)
         test_accuracy = Model.evaluate(test_iter)
-        if epoch % report_every == 0 and epoch != EPOCHS:
+        if epoch % report_every == 0 and epoch != epochs:
             print(f"Epoch[{str(epoch).rjust(2)}] --- Loss: [{loss:.3f}],", \
                   f"Train Accuracy: [{train_accuracy:.3f}]," \
                   f"Test Accuracy: [{test_accuracy:.3f}]")
@@ -93,3 +79,7 @@ Test Accuracy: [{Model.evaluate(test_iter):.5f}]", end='\n\n')
     print(f"Weights: {Model.linear.weight.reshape(-1)}")
     print(f"Bias: {Model.linear.bias.reshape(-1)}")
     print()
+
+
+if __name__ == '__main__':
+    main()
